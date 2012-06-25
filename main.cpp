6452716515bbf022a153b93cff432593a2c7f63c
Tyler O'Neill <metasiah@gmail.com>
2012-06-25 18:16:44 -0500

    tersified

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <memory>
 #include <iostream>
+#include <fstream>
 
 const std::string STD_LIB = 
 #ifndef NDEBUG
@@ -25,20 +26,44 @@ private:
 
 BrainFucktor Compile(const std::string &code);
 std::string BrainfuckIgnore(const std::string &code);
+unsigned short HiWord(unsigned int dword);
+unsigned short LoWord(unsigned int dword);
+unsigned char HiByte(unsigned short word);
+unsigned char LoByte(unsigned short word);
 
 int main()
 {    
-  BrainFucktor func = Compile("++[>++[>++[>++[>++<-] <-] <-] <-]  >>>><+++++++++++[>+++++<-] >. <++[>++++++++<-] >+. <+++[>--<-] >-. <+++[>++++++<-] >+."
-    "<+++++++++++[>-------<-] >. <+++++++++++++++++++[>++++<-] >. <+++++++++++++++++++++++++++++++++++++++++[>--<-] >-. <+++++++++++++++++[>+++++<-] >."
-    "<++[>--<-] >-. <+++++[>----------------<-] >. <+++++++++++++++++++[>++<-] >. <+++++[>++<-] >. <++[>--------<-] >-. ");
-  func();  
+  std::ifstream ifs("test.b");
+  if(!ifs)
+    return 1;
+  std::string code;
+  while(!ifs.eof())
+  {
+    std::string line;
+    std::getline(ifs, line);
+    code += line;
+  }
+  Compile(code)();  
   std::cin.get();
   return 0;  
 }
 
 BrainFucktor Compile(const std::string &code)
 {
-  static const std::size_t BUFFER_SIZE = 16384;
+  static const std::size_t BUFFER_SIZE = 16384; // size of allocated function; not of function data (that's fixed to 3000 bytes)
+  static const unsigned char PROLOG[] = "\x55\x89\xe5\x81\xec\x90\x0c\x00\x00\x8d\x85\x40\xf4\xff\xff\x89"
+    "\x85\x34\xf4\xff\xff\xc6\x85\x44\xff\xff\xff\x00\x68\xb7\x0b\x00\x00\x6a\x00\x8d\x85\x41\xf4\xff\xff"
+    "\x48\x50\xe8";
+  static const unsigned char INC_DATA[] = "\x8b\x85\x34\xf4\xff\xff\x8a\x08\xfe\xc1\x8b\x95\x34\xf4\xff\xff\x88\x0a";
+  static const unsigned char DEC_DATA[] = "\x8b\x85\x34\xf4\xff\xff\x8a\x08\xfe\xc9\x8b\x95\x34\xf4\xff\xff\x88\x0a";
+  static const unsigned char INC_DP[] = "\x8b\x85\x34\xf4\xff\xff\x40\x89\x85\x34\xf4\xff\xff";
+  static const unsigned char DEC_DP[] = "\x8b\x85\x34\xf4\xff\xff\x48\x89\x85\x34\xf4\xff\xff";
+  static const unsigned char PUTCHAR_PROLOG[] = "\x8b\x85\x34\xf4\xff\xff\x0f\xbe\x08\x51\xe8";
+  static const unsigned char PUTCHAR_EPILOG[] = "\x83\xc4\x04\x8b\x85\x34\xf4\xff\xff";
+  static const unsigned char GETCHAR_PROLOG[] = "\xe8";
+  static const unsigned char GETCHAR_EPILOG[] = "\x8b\x8d\x34\xf4\xff\xff\x88\x01\x8b\x85\x34\xf4\xff\xff";
+  static const unsigned char LOOP_START[] = "\x8b\x8d\x34\xf4\xff\xff\x0f\xbe\x08\x85\xc9\x0f\x84\x00\x00\x00\x00";  
+  static const unsigned char EPILOG[] = "\x89\xec\x5d\xc3";
 
   std::string cleanedCode = BrainfuckIgnore(code);
 
@@ -52,28 +77,18 @@ BrainFucktor Compile(const std::string &code)
   void *buffer = operator new(BUFFER_SIZE);
   unsigned char *walker = reinterpret_cast<unsigned char*>(buffer);
   std::size_t idx = 0;
-#define add_opcode(opcode) walker[idx++] = opcode
-  // PROLOG
-  add_opcode(0x55); // push ebp
-  add_opcode(0x89); add_opcode(0xE5); // mov ebp, esp  
-  add_opcode(0x81); add_opcode(0xEC); add_opcode(0x90); add_opcode(0x0C); add_opcode(0x00); add_opcode(0x00); // sub esp, 0xc90  
-  add_opcode(0x8D); add_opcode(0x85); add_opcode(0x40); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // lea eax, [ebp-0xbc0]
-  add_opcode(0x89); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov dword ptr [ebp-0xbcc], eax
 
-  add_opcode(0xc6); add_opcode(0x85); add_opcode(0x44); add_opcode(0xff); add_opcode(0xff); add_opcode(0xff); add_opcode(0x00); // mov byte ptr [ebp-0xbc], 0
-  add_opcode(0x68); add_opcode(0xb7); add_opcode(0x0b); add_opcode(0x00); add_opcode(0x00); // push 0xbb7
-  add_opcode(0x6a); add_opcode(0x00); // push 0
-  add_opcode(0x8D); add_opcode(0x85); add_opcode(0x41); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // lea eax, [ebp-0xbbf]
-  add_opcode(0x48); // dec eax
-  add_opcode(0x50); // push eax
-  add_opcode(0xe8); 
+#define add_opcode(opcode) walker[idx++] = opcode
+#define add_opcode_block(block) memcpy(walker + idx, (block), sizeof((block)) - 1); \
+  idx += sizeof(block) - 1
+#define add_dword(dword) add_opcode(LoByte(LoWord((dword)))); add_opcode(HiByte(LoWord((dword)))); \
+  add_opcode(LoByte(HiWord((dword)))); add_opcode(HiByte(HiWord((dword))))
+  
+  add_opcode_block(PROLOG);
   
   UINT_PTR memsetLoc = (memsetIlt - reinterpret_cast<UINT_PTR>(buffer)) - idx - 0x04;
-
-  add_opcode(LOBYTE(LOWORD(memsetLoc))); add_opcode(HIBYTE(LOWORD(memsetLoc))); 
-  add_opcode(LOBYTE(HIWORD(memsetLoc))); add_opcode(HIBYTE(HIWORD(memsetLoc))); // call memset
-  add_opcode(0x83); add_opcode(0xc4); add_opcode(0x0c); // add esp, 0x0c    
-  // PROLOG
+  add_dword(memsetLoc);
+  add_opcode(0x83); add_opcode(0xc4); add_opcode(0x0c);
 
   std::stack<std::pair<unsigned int*, std::size_t>> loops;
 
@@ -82,67 +97,30 @@ BrainFucktor Compile(const std::string &code)
   {
     switch(*it)
     {
-    case '+':
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
-      add_opcode(0x8A); add_opcode(0x08); // mov cl, byte ptr [eax]
-      add_opcode(0xFE); add_opcode(0xC1); // inc cl
-      add_opcode(0x8B); add_opcode(0x95); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov edx, dword ptr [ebp-0xbcc]
-      add_opcode(0x88); add_opcode(0x0A); // mov byte ptr [edx], cl
-      break;
-    case '-':
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
-      add_opcode(0x8A); add_opcode(0x08); // mov cl, byte ptr [eax]
-      add_opcode(0xFE); add_opcode(0xC9); // dec cl
-      add_opcode(0x8B); add_opcode(0x95); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov edx, dword ptr [ebp-0xbcc]
-      add_opcode(0x88); add_opcode(0x0A); // mov byte ptr [edx], cl
-      break;
-    case '>':
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
-      add_opcode(0x40); // inc eax
-      add_opcode(0x89); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov dword ptr [ebp-0xbcc], eax
-      break;
-    case '<':
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
-      add_opcode(0x48); // dec eax
-      add_opcode(0x89); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov dword ptr [ebp-0xbcc], eax
-      break;
+    case '+': add_opcode_block(INC_DATA); break;
+    case '-': add_opcode_block(DEC_DATA); break;
+    case '>': add_opcode_block(INC_DP); break;
+    case '<': add_opcode_block(DEC_DP); break;
     case '.':
       {
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
-      add_opcode(0x0f); add_opcode(0xbe); add_opcode(0x08); // movsx ecx, byte ptr [eax]
-      add_opcode(0x51); // push ecx
-
-      add_opcode(0xe8);
+      add_opcode_block(PUTCHAR_PROLOG);
       UINT_PTR putcharLoc = (putcharIlt - reinterpret_cast<UINT_PTR>(buffer)) - idx - 0x04;
-
-      add_opcode(LOBYTE(LOWORD(putcharLoc))); add_opcode(HIBYTE(LOWORD(putcharLoc))); 
-      add_opcode(LOBYTE(HIWORD(putcharLoc))); add_opcode(HIBYTE(HIWORD(putcharLoc))); // call putchar
-
-      add_opcode(0x83); add_opcode(0xc4); add_opcode(0x04); // add esp, 4
-
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
+      add_dword(putcharLoc);
+      add_opcode_block(PUTCHAR_EPILOG);
       }
       break;
     case ',':
       {
-      add_opcode(0xe8);      
+      add_opcode_block(GETCHAR_PROLOG);
       UINT_PTR getcharLoc = (getcharIlt - reinterpret_cast<UINT_PTR>(buffer)) - idx - 0x04;      
-
-      add_opcode(LOBYTE(LOWORD(getcharLoc))); add_opcode(HIBYTE(LOWORD(getcharLoc))); 
-      add_opcode(LOBYTE(HIWORD(getcharLoc))); add_opcode(HIBYTE(HIWORD(getcharLoc))); // call getchar
-      add_opcode(0x8B); add_opcode(0x8d); add_opcode(0x34); add_opcode(0xf4); add_opcode(0xff); add_opcode(0xff); // mov ecx, dword ptr [ebp-0xbcc]
-      add_opcode(0x88); add_opcode(0x01); // mov byte ptr [ecx], al
-      add_opcode(0x8B); add_opcode(0x85); add_opcode(0x34); add_opcode(0xF4); add_opcode(0xFF); add_opcode(0xFF); // mov eax, dword ptr [ebp-0xbcc]
+      add_dword(getcharLoc);
+      add_opcode_block(GETCHAR_EPILOG);
       }
       break;
     case '[':            
-      {
-      unsigned int *endLoop = 0;
-      std::pair<unsigned int*, std::size_t> pair(endLoop, idx);
-      add_opcode(0x8B); add_opcode(0x8d); add_opcode(0x34); add_opcode(0xf4); add_opcode(0xff); add_opcode(0xff); // mov ecx, dword ptr [ebp-0xbcc]
-      add_opcode(0x0f); add_opcode(0xbe); add_opcode(0x08); // movsx ecx, byte ptr [eax]      
-      add_opcode(0x85); add_opcode(0xc9); // test ecx, ecx
-      add_opcode(0x0f); add_opcode(0x84); add_opcode(0x00); add_opcode(0x00); add_opcode(0x00); add_opcode(0x00); // je short ]
+      {      
+      std::pair<unsigned int*, std::size_t> pair(static_cast<unsigned int*>(0), idx);
+      add_opcode_block(LOOP_START);
       pair.first = reinterpret_cast<unsigned int*>(&walker[idx-4]);
       loops.push(pair);
       }
@@ -152,22 +130,18 @@ BrainFucktor Compile(const std::string &code)
       std::pair<unsigned int*, std::size_t> pair = loops.top();
       loops.pop();
       unsigned int offset = 0xFFFFFFFF - (idx - pair.second) - 4;
-      add_opcode(0xe9); add_opcode(LOBYTE(LOWORD(offset))); add_opcode(HIBYTE(LOWORD(offset))); add_opcode(LOBYTE(HIWORD(offset)));
-        add_opcode(HIBYTE(HIWORD(offset))); // jmp short [
-      *pair.first = static_cast<unsigned int>(idx - pair.second) - 17;
+      add_opcode(0xe9); add_dword(offset);
+      *pair.first = static_cast<unsigned int>(idx - pair.second) - sizeof(LOOP_START) + 1;
       }
       break;
-    default: _asm int 3;
+    default: _asm int 3; // literally impossible with BrainfuckIgnore
     }
   }
 
-  // EPILOG
-  add_opcode(0x89); 
-  add_opcode(0xEC); // mov esp, ebp
-  add_opcode(0x5D); // pop ebp
-  add_opcode(0xC3); // ret
-  // EPILOG
+  add_opcode_block(EPILOG);
 #undef add_opcode
+#undef add_opcode_block
+#undef add_dword
 
   DWORD old;
   VirtualProtect(buffer, BUFFER_SIZE, PAGE_EXECUTE_READWRITE, &old);
@@ -184,4 +158,24 @@ std::string BrainfuckIgnore(const std::string &code)
       result += *it;
   }
   return result;
+}
+
+unsigned short HiWord(unsigned int dword)
+{
+  return static_cast<unsigned short>(dword >> 16);
+}
+
+unsigned short LoWord(unsigned int dword)
+{
+  return static_cast<unsigned short>(dword);
+}
+
+unsigned char HiByte(unsigned short word)
+{
+  return static_cast<unsigned char>(word >> 8);
+}
+
+unsigned char LoByte(unsigned short word)
+{
+  return static_cast<unsigned char>(word);
 }
